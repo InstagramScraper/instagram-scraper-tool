c8f92845c3c1e68f010f7f4591f9a71cc95c99c1
Luca <instascrapper2021@gmail.com>
2021-02-01 04:00:45 +0100

    added save data to excel methods

--- lib/instagram_scraper.py
+++ lib/instagram_scraper.py
@@ -1,6 +1,8 @@
+import os
 import time
 import json
 import requests
+import xlsxwriter
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,11 +11,23 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 class InstagramScraper:
-    def __init__(self, webdriver_path, instagram_url="http://www.instagram.com"):
+    def __init__(self, webdriver_path, instagram_url="http://www.instagram.com", output_folder="output"):
         self.instagram_url=instagram_url
         self.webdriver_path=webdriver_path
         self.getDriver()
         self.driver.get(self.instagram_url)
+        self.output_folder=output_folder
+        self.makeDir(self.output_folder)
+        self.currentPage=""
+        self.all_posts_comments=[]
+        self.all_posts_likes=[]
+        self.all_posts_captions=[]
+        self.all_posts_hashtag = []
+
+    def makeDir(self,folder_name): 
+        '''Make a dir'''
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
     
     def getDriver(self):
         '''Load the webdriver for Selenium'''
@@ -55,8 +69,16 @@ class InstagramScraper:
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Not Now")]'))).click()
 
+    def clean(self):
+        self.all_posts_comments=[]
+        self.all_posts_likes=[]
+        self.all_posts_captions=[]
+        self.all_posts_hashtag = []
+
     def openInstagramPage(self,page_name):
         '''Surf to an Instagram page'''
+        self.currentPage=page_name
+        self.clean()
         return self.openWebPage(self.instagram_url + "/" + page_name + "/")
 
     def getNumberOfPosts(self, page_name=None):
@@ -68,6 +90,7 @@ class InstagramScraper:
     
     def getPostsUrls(self, limit = None, page_name=None):
         '''Return a list containing the url of all the posts of an Instagram page'''
+        self.clean()
         if page_name!=None:
             self.openInstagramPage(page_name)
         time.sleep(3)
@@ -104,6 +127,7 @@ class InstagramScraper:
             - "comments" = (list) list containing all the comments
             - "number_of_likes" = (int) number of like
             - "media_url" = (str) url of the media (image or video)
+            - "hashtags" = (list) list containing all the hashtags
             for a specific post specified througt post_url parameter
         '''
         self.openWebPage(post_url)
@@ -111,27 +135,96 @@ class InstagramScraper:
         data = json.loads(text)
         post_data = dict()
         post_data["caption"] = data["graphql"]["shortcode_media"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
-        post_data["number_of_comments"] = data["graphql"]["shortcode_media"]["edge_media_to_parent_comment"]["count"]
+        post_data["hashtags"] = [ w for w in post_data["caption"].split(" ") if w[0]=="#"]
+        post_data["number_of_comments"] = int(data["graphql"]["shortcode_media"]["edge_media_to_parent_comment"]["count"])
         post_data["is_video"] = data["graphql"]["shortcode_media"]["is_video"]
         post_data["comments"] = data["graphql"]["shortcode_media"]["edge_media_to_parent_comment"]["edges"]
-        post_data["number_of_likes"] = data["graphql"]["shortcode_media"]["edge_media_preview_like"]
+        post_data["number_of_likes"] = int(data["graphql"]["shortcode_media"]["edge_media_preview_like"]["count"])
         if (post_data["is_video"]):
             post_data["media_url"] = data["graphql"]["shortcode_media"]["video_url"]
         else:
             post_data["media_url"] = data["graphql"]["shortcode_media"]["display_url"]
+        self.all_posts_likes.append(post_data["number_of_likes"])
+        self.all_posts_comments.append(post_data["comments"])
+        self.all_posts_captions.append(post_data["caption"])
+        self.all_posts_hashtag.append(post_data["hashtags"])
         return post_data
     
-    def saveMediaToFile(self, post_url, output_filename_without_format):
+    def saveMediaToFile(self, media_url, is_video,output_filename_without_format):
         '''Save the media of a post to file'''
-        post_data = self.getPostData(post_url)
-        reponse = requests.get(post_data["media_url"])
+        self.makeDir(self.output_folder)
+        self.makeDir(self.output_folder + os.sep + self.currentPage) 
+        self.makeDir(self.output_folder + os.sep + self.currentPage + os.sep + "media") 
+        reponse = requests.get(media_url)
         if reponse.status_code == 200:
-            if (post_data["is_video"]):
+            if (is_video):
                 file_format = ".mp4"
             else:
                 file_format=".jpg"
-            with open(output_filename_without_format +file_format,"wb") as file:
+            with open(self.output_folder + os.sep + self.currentPage + os.sep + "media" + os.sep + output_filename_without_format +file_format,"wb") as file:
                 file.write(reponse.content)
             return True
         else:
             return False
+
+    def saveAllCommentsToExcel(self, output_filename_without_format):
+        self.makeDir(self.output_folder)
+        self.makeDir(self.output_folder + os.sep + self.currentPage)
+        workbook = xlsxwriter.Workbook(self.output_folder + os.sep + self.currentPage + os.sep + output_filename_without_format + ".xlsx")
+        worksheet = workbook.add_worksheet()
+        col = 0
+        for comments in self.all_posts_comments:
+            worksheet.write(0, col, "Comments_"+"{:06n}".format(col))
+            row = 1
+            for comment in comments:
+                worksheet.write(row, col, comment["node"]["text"])
+                row += 1
+            col+=1
+        workbook.close()
+
+    def saveAllCaptionsToExcel(self, output_filename_without_format):
+        self.makeDir(self.output_folder)
+        self.makeDir(self.output_folder + os.sep + self.currentPage)
+        workbook = xlsxwriter.Workbook(self.output_folder + os.sep + self.currentPage + os.sep + output_filename_without_format + ".xlsx")
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 0, "Post ID")
+        worksheet.write(0, 1, "Caption")
+        row = 1
+        post_id = 0
+        for caption in self.all_posts_captions:
+            worksheet.write(row, 0, "{:06n}".format(post_id))
+            worksheet.write(row, 1, caption)
+            row += 1
+            post_id +=1
+        workbook.close()
+    
+    def saveAllHashtagsToExcel(self, output_filename_without_format):
+        self.makeDir(self.output_folder)
+        self.makeDir(self.output_folder + os.sep + self.currentPage)
+        workbook = xlsxwriter.Workbook(self.output_folder + os.sep + self.currentPage + os.sep + output_filename_without_format + ".xlsx")
+        worksheet = workbook.add_worksheet()
+        col = 0
+        for hs in self.all_posts_hashtag:
+            worksheet.write(0, col, "Hashtags_"+"{:06n}".format(col))
+            row = 1
+            for h in hs:
+                worksheet.write(row, col, h)
+                row += 1
+            col+=1
+        workbook.close()
+    
+    def saveAllLikesToExcel(self, output_filename_without_format):
+        self.makeDir(self.output_folder)
+        self.makeDir(self.output_folder + os.sep + self.currentPage)
+        workbook = xlsxwriter.Workbook(self.output_folder + os.sep + self.currentPage + os.sep + output_filename_without_format + ".xlsx")
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 0, "Post ID")
+        worksheet.write(0, 1, "#Likes")
+        row = 1
+        post_id = 0
+        for likes in self.all_posts_likes:
+            worksheet.write(row, 0, "{:06n}".format(post_id))
+            worksheet.write(row, 1, likes)
+            row += 1
+            post_id +=1
+        workbook.close()
